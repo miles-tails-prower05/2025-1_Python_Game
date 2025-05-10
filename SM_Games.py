91e6140c0354428de5d3e99641588f7a97546bec
miles-tails-prower05 <kunwoopark05@gmail.com>
2025-05-11 00:56:38 +0900

    화면 비율 변경, 텍스트 위젯 추가

--- SM_Games.py
+++ SM_Games.py
@@ -3,12 +3,16 @@
 root = Tk()
 root.configure(bg="white")
 root.title("무인도 살인사건 추리게임")
-root.geometry("1090x1080")
+root.geometry("1700x900")
 
 canvas = Canvas(root, width=700, height=700)
 canvas.pack()
 
-scrollbar=Tk.Scrollbar(root)
+text = Text(root, width=15, height=12)
+scrollbar = Scrollbar(root, command=text.yview)
+text.configure(yscrollcommand=scrollbar.set)
+
+text.pack(sid="left")
 scrollbar.pack(side="right", fill="y")
 
 root.mainloop()
