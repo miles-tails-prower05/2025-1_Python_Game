fb76cabf2e4d70c75f373b1fc551aa6da46164e4
miles-tails-prower05 <kunwoopark05@gmail.com>
2025-05-10 22:41:18 +0900

    인코딩 설정 변경

---
+++ SM_Games.py
@@ -0,0 +1,14 @@
+﻿from tkinter import *
+
+root = Tk()
+root.configure(bg="white")
+root.title("무인도 살인사건 추리게임")
+root.geometry("1090x1080")
+
+canvas = Canvas(root, width=700, height=700)
+canvas.pack()
+
+scrollbar=Tk.Scrollbar(root)
+scrollbar.pack(side="right", fill="y")
+
+root.mainloop()
